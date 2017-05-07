26540a37c10a9828e91a27c81e561c87b3171232
Luciano Barros <ferreira.obarros@gmail.com>
2017-05-07 17:26:19 -0300

    versao 1.0 - Configuração de Banco de Dados - Nova Base

--- src/main/java/com/br/pb/barros/avaliabus/conf/AppWebConfiguration.java
+++ src/main/java/com/br/pb/barros/avaliabus/conf/AppWebConfiguration.java
@@ -46,7 +46,7 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	@Bean(name = "dataSource") public DriverManagerDataSource dataSource() {
 		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
 		driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		driverManagerDataSource.setUrl("jdbc:mysql://avaliabus:3306/sampledb");
+		driverManagerDataSource.setUrl("jdbc:mysql://mysql:3306/sampledb");
 		driverManagerDataSource.setUsername("root");
 		driverManagerDataSource.setPassword("uKrwbCEXNFtJ1gAo");
 		
--- src/main/java/com/br/pb/barros/avaliabus/conf/JPAConfiguration.java
+++ src/main/java/com/br/pb/barros/avaliabus/conf/JPAConfiguration.java
@@ -36,7 +36,7 @@ public class JPAConfiguration {
 	@Bean public DataSource dataSource(Environment environment) {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://avaliabus:3306/sampledb");
+		dataSource.setUrl("jdbc:mysql://mysql:3306/sampledb");
 		dataSource.setUsername("root");
 		dataSource.setPassword("uKrwbCEXNFtJ1gAo");
 		/*dataSource.setUrl("jdbc:mysql://localhost/avaliabus");

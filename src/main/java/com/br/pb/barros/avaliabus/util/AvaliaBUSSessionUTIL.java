851482263430a5a4eee7b7119b65b715930d028e
Luciano Barros <ferreira.obarros@gmail.com>
2017-06-01 19:50:55 -0300

    versao 1.0 - Apresentação Final

--- src/main/java/com/br/pb/barros/avaliabus/util/AvaliaBUSSessionUTIL.java
+++ src/main/java/com/br/pb/barros/avaliabus/util/AvaliaBUSSessionUTIL.java
@@ -15,7 +15,7 @@ public class AvaliaBUSSessionUTIL {
 	
 	private boolean isLogado;
 	
-	private boolean isLocal = true;
+	private boolean isLocal = false;
 	
 	public boolean isAdminMaster(){
 		return isAdminMaster;
